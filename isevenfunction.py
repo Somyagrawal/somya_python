1e7f478fb7d21290947000c03436b2890012dfbc
somyagrawal <divyaprakash82@gmail.com>
2024-08-24 12:43:24 +0530

    first commt

---
+++ isevenfunction.py
@@ -0,0 +1,20 @@
+def is_even1(n):
+    if n%2==0:
+        return True
+    else:
+        return False
+
+def is_even(n):
+    return n%2==0
+
+  
+s=int(input("Enter a number"))
+if is_even(s):
+   print("even")
+else:
+    print("odd")
+    
+     
+
+
+
