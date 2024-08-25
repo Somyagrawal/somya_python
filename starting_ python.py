f10211d879a9c2392d865222b76cc66fb86554aa
somyagrawal <divyaprakash82@gmail.com>
2024-08-25 12:20:37 +0530

    add readabilty folder

---
+++ starting_ python.py
@@ -0,0 +1,6 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=12
+>>> b=13
+>>> c=10
+>>> d=a+b+c
